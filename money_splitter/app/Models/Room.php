a6ef4344fcab2220f78e4e323d8d634ff472d8ae
Rysul Nirob <rysulamannirob@gmail.com>
2025-10-10 16:33:39 +0600

    added migration and model tables for rooms and relation with user

---
+++ money_splitter/app/Models/Room.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['creater_id', 'name'];
+
+    public function creater()
+    {
+        return $this->belongsTo(User::class, 'creater_id');
+    }
+
+    public function members()
+    {
+        return $this->belongsToMany(User::class, 'room_members', 'room_id', 'member_id');
+    }
+}
